f312f894cba3a7f7a458f931c5f7b37ae3775107
VG-Dricovw <dricovanwijk@vertigo-games.com>
2024-08-27 12:23:13 +0200

    save for lunch

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Question;
+
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +18,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'quizmaster',
+        //     'email' => 'quiz@example.com',
+        // ]);
+
+        Question::factory()->count(5)->make();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::resource('users', UserController::class);
+
+Route::get('/user/{id}', function (string $id) {
+    return new UserResource(User::findOrFail($id));
 });
